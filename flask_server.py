4e845ab20252ce2a7b464073ba6abf4ee0560614
Kailash <ahikailash1@gmail.com>
2024-11-16 22:03:44 +0530

    Flask server updated;secret key added

--- flask_server.py
+++ flask_server.py
@@ -2,16 +2,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+import os
+
 from flask import Flask, request, jsonify
 
 from run_base_agent import run_agent
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get("FLASK_SECRET_KEY")
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def generate():
-    response = run_agent(request.form.get('prompt'))
-    return jsonify({"response": response})
+    if request.method == "POST":
+        response = run_agent(request.form.get('prompt'))
+        return jsonify({"response": response})
+    else:
+        return jsonify({"status":"failure", "message": "invalid call!"})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
