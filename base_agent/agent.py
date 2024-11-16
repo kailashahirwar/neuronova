738cb45562f7b626da09359913d9681efa839c2b
Kailash <ahikailash1@gmail.com>
2024-11-17 01:24:19 +0530

    Default prompt added

--- base_agent/agent.py
+++ base_agent/agent.py
@@ -3,10 +3,12 @@ import random
 import uuid
 from cdp_langchain.agent_toolkits import CdpToolkit
 from cdp_langchain.utils import CdpAgentkitWrapper
+from click import prompt
 from langchain.tools import Tool
 from langchain_community.document_loaders import CSVLoader
 from langchain_community.vectorstores import Chroma
 from langchain_core.messages import HumanMessage
+from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
 from langgraph.checkpoint.memory import MemorySaver
@@ -113,7 +115,7 @@ def create_agent(llm, wallet_details, tools, csv_agent=False, csv_file_path=None
             return results
 
         retrieval_tool = Tool(
-            name="DataRetriever",
+            name="csv_data_retriever_tool",
             func=retrieve_data,
             description="Use this tool to retrieve relevant information from the CSV file."
         )
@@ -130,9 +132,15 @@ def create_agent(llm, wallet_details, tools, csv_agent=False, csv_file_path=None
                        "If not, you can provide your wallet details and request funds from the user. "
                        "If someone asks you to do something you can't do with your currently available tools, "
                        "you must say so, and encourage them to implement it themselves using the CDP SDK + Agentkit,"
-                       " recommend they go to docs.cdp.coinbase.com for more information. Be concise and helpful with "
+                       "recommend they go to docs.cdp.coinbase.com for more information. Be concise and helpful with "
                        "your responses. Refrain from restating your tools' descriptions unless it is "
                        "explicitly requested."
+                       "Historical ETH prices are in the prices.csv file. The ETH column shows ETH prices. "
+                       "The time column shows time in the %Y-%m-%d %H:%M:%S %Z Python datetime format. "
+                       "Data is listed from oldest to newest. "
+                       "You have get_spot_price_tool and csv_data_retriever_tool available."
+                       "Use the get_spot_price_tool and csv_data_retriever_tool for this task."
+                       "Recommend trade options to the user. Be clear and straightforward in your suggestions."
     )
 
     return agent
@@ -147,3 +155,27 @@ def create_memory():
     memory = MemorySaver()
 
     return memory
+
+def get_default_prompt():
+    template = """Answer the following questions as best you can. You have access to the following tools:
+
+    {tools}
+
+    Use the following format:
+
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+
+    Begin!
+
+    Question: {input}
+    Thought:{agent_scratchpad}"""
+
+    prompt = PromptTemplate.from_template(template)
+    return prompt
